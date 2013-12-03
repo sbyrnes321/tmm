e77cd936b705cb028557422c2dc951599f98e9e9
Steve <steven.byrnes@gmail.com>
2013-12-03 10:53:42 -0500

    New pypi upload: Version 0.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ data_files = ['README.txt','LICENSE.txt','Changes.txt','manual.pdf']
 
 setup(
     name = "tmm",
-    version = '0.1.1',
+    version = '0.1.2',
     author = "Steven Byrnes",
     author_email = "steven.byrnes@gmail.com",
     description = descrip,
